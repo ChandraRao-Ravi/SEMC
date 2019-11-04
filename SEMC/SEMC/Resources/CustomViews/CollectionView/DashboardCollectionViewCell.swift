eca8e8515e9d49b81157de113b6dccb64c32ac23
Chandra Rao <Chandrarao.ravi@gmail.com>
2019-11-05 02:24:45 +0530

    Added Code for Review

---
+++ SEMC/SEMC/Resources/CustomViews/CollectionView/DashboardCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  DashboardCollectionViewCell.swift
+//  SEMC
+//
+//  Created by Chandra Rao on 21/10/19.
+//  Copyright © 2019 Chandra Rao. All rights reserved.
+//
+
+import UIKit
+
+class DashboardCollectionViewCell: UICollectionViewCell {
+    
+}
